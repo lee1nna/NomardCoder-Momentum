ddec11ba549803c9086214dfa8aa2c8f3e8e9630
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2021-06-28 18:39:06 +0900

    add calculator

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,6 +1,19 @@
-function plus(a, b)
-{
-    console.log(a + b);
+const calculator = {
+    add: function(a, b){
+        console.log(a + b);
+    },
+    abstract: function(a, b){
+        console.log(a - b);
+    },
+    multiple: function(a, b){
+        console.log(a * b);
+    },
+    divide: function(a, b){
+        console.log(a / b);
+    }
 }
 
-plus(2, 4);
+calculator.add(2, 4);
+calculator.abstract(2, 4);
+calculator.multiple(2, 4);
+calculator.divide(2, 4);
