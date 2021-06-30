bea9083ef05231bc4419f9e70a73c19e85e7a6fb
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2021-06-30 23:52:25 +0900

    js conditional basic

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,19 +1,5 @@
-const calculator = {
-    add: function(a, b){
-        console.log(a + b);
-    },
-    abstract: function(a, b){
-        console.log(a - b);
-    },
-    multiple: function(a, b){
-        console.log(a * b);
-    },
-    divide: function(a, b){
-        console.log(a / b);
-    }
-}
+const age = parseInt(prompt("How old are you?"));
 
-calculator.add(2, 4);
-calculator.abstract(2, 4);
-calculator.multiple(2, 4);
-calculator.divide(2, 4);
+if(isNaN(age)){
+    console.log("Please write a number");
+}
