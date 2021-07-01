19af7f608bc584b5b9d599245978894b6d614a24
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2021-07-01 23:13:36 +0900

    bring html element

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,15 +1,20 @@
-const age = parseInt(prompt("How old are you?"));
+// id값을 통해 html의 element를 가져오는 방식
+// const title = document.getElementById("title");
+// title.innerText = "Got you!";
+// console.log(title.id);
+// console.log(title.className);
 
-if(isNaN(age) || age < 0){
-    console.log("Please write a real positive number.");
-} else if(age < 19){
-    console.log("You are too young.");
-} else if(age >= 18 && age <= 50){
-    console.log("You can drink.");
-} else if(age > 50 && age <= 80){
-    console.log("You should exercise.")
-} else if(age >= 80 && age < 100){
-    console.log("You can do whatever you want.");
-} else if(age === 100){
-    console.log("wow you are wise.");
-}
+// classname을 통해 html의 element를 가져오는 방식
+// const hellos = document.getElementsByClassName("hello");
+// console.log(hellos);
+
+// tagname을 통해 html의 element를 가져오는 방식
+// const title = document.getElementsByTagName("h1");
+// console.log(title);
+
+// querySelector는 element를 CSS 방식으로 검색할 수 있음.
+// 단 하나의 element를 return 해 줌. (가장 첫번째 element)
+// 조건에 부합하는 모든 element를 검색하고 싶다면 querySelectorAll 사용
+const title = document.querySelector(".hello h1");
+
+title.innerText = "hello";
