7fd1d5cc6717847871c896e144eebafd1ae1d8e7
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2021-07-01 23:27:28 +0900

    click event

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,20 +1,7 @@
-// id값을 통해 html의 element를 가져오는 방식
-// const title = document.getElementById("title");
-// title.innerText = "Got you!";
-// console.log(title.id);
-// console.log(title.className);
+const title = document.querySelector("div.hello:first-child h1");
 
-// classname을 통해 html의 element를 가져오는 방식
-// const hellos = document.getElementsByClassName("hello");
-// console.log(hellos);
+function handleTitleClick(){
+    title.style.color = "blue";
+}
 
-// tagname을 통해 html의 element를 가져오는 방식
-// const title = document.getElementsByTagName("h1");
-// console.log(title);
-
-// querySelector는 element를 CSS 방식으로 검색할 수 있음.
-// 단 하나의 element를 return 해 줌. (가장 첫번째 element)
-// 조건에 부합하는 모든 element를 검색하고 싶다면 querySelectorAll 사용
-const title = document.querySelector(".hello h1");
-
-title.innerText = "hello";
+title.addEventListener("click", handleTitleClick);
