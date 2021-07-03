ea5ea9f999dec338237cc06795e1b0c84220c36a
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2021-07-04 02:54:05 +0900

    role distribution

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,37 +1,11 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleh1Click(){
-    h1.style.color = "blue";
+    if(h1.className ==="active") {
+        h1.className = "";
+    } else {
+        h1.className = "active";
+    }
 }
 
-function handleMouseEneter(){
-    h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave(){
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize(){
-    document.body.style.background = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!");
-}
-
-function handleWindowOffline(){
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-    alert("ALL Good");
-}
 h1.addEventListener("click", handleh1Click);
-h1.addEventListener("mouseenter", handleMouseEneter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
