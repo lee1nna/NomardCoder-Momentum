f27c1ae46d670403cb89263fb0a31c39258b185f
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2021-07-04 02:19:26 +0900

    add event listener

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -4,4 +4,14 @@ function handleTitleClick(){
     title.style.color = "blue";
 }
 
+function handleMouseEneter(){
+    title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave(){
+    title.innerText = "Mouse is gone!";
+}
+
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEneter);
+title.addEventListener("mouseleave", handleMouseLeave);
