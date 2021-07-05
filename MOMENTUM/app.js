c14422f40b8604fb8af4169501e19486ed5daf05
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2021-07-05 23:50:58 +0900

    add loginform

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,11 +1,1 @@
-const h1 = document.querySelector("div.hello:first-child h1");
-
-function handleh1Click(){
-    if(h1.className ==="active") {
-        h1.className = "";
-    } else {
-        h1.className = "active";
-    }
-}
-
-h1.addEventListener("click", handleh1Click);
+const loginForm = document.querySelector("#login-form");
